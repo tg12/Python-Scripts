a7165be7729ee9e8d72bfb70c846921522f1bc3a
tg12 <tg12@users.noreply.github.com>
2019-07-16 15:50:28 +0100

    Add files via upload

---
+++ outliers.py
@@ -0,0 +1,23 @@
+import numpy
+import random
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+# arr = [10, 386, 479, 627, 20, 523, 482, 483, 542, 699, 535, 617, 577, 471, 615, 583, 441, 562, 563, 527, 453, 530, 433, 541, 585, 704, 443, 569, 430, 637, 331, 511, 552, 496, 484, 566, 554, 472, 335, 440, 579, 341, 545, 615, 548, 604, 439, 556, 442, 461, 624, 611, 444, 578, 405, 487, 490, 496, 398, 512, 422, 455, 449, 432, 607, 679, 434, 597, 639, 565, 415, 486, 668, 414, 665, 763, 557, 304, 404, 454, 689, 610, 483, 441, 657, 590, 492, 476, 437, 483, 529, 363, 711, 543]
+
+arr = [random.randint(0, 36) for x in range(1, 700000)]
+
+elements = numpy.array(arr)
+
+mean = numpy.mean(elements, axis=0)
+sd = numpy.std(elements, axis=0)
+
+final_list = [x for x in arr if (x > mean - 2 * sd)]
+final_list = [x for x in final_list if (x < mean + 2 * sd)]
+print(final_list)
+
+c = Counter(final_list)
+
+plt.bar(c.keys(), c.values())
+plt.show()
---
+++ temper2.py
@@ -0,0 +1,83 @@
+from __future__ import division
+import sys
+import time
+import hid
+
+
+class Temper2:
+
+    vendor_id = 0x413D
+    product_id = 0x2107
+    name_query = [0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00]
+    temp_query = [0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00]
+    max_open_tries = 10
+    read_timeout = 500
+
+    def __init__(self):
+        self.h = hid.device()
+        is_open = False
+        tries = 0
+        while not is_open:
+            try:
+                tries += 1
+                self.h.open(self.vendor_id, self.product_id)
+            except BaseException:
+                if tries >= self.max_open_tries:
+                    raise IOError('could not open device')
+                time.sleep(0.1)
+                continue
+            is_open = True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.h.close()
+
+    def get_name(self):
+        self.write(self.name_query)
+        resp1 = self.read(8, self.read_timeout)
+        resp2 = self.read(8, self.read_timeout)
+        resp1_chars = self._read_string(resp1)
+        resp2_chars = self._read_string(resp2)
+        return resp1_chars + resp2_chars
+
+    def get_temp(self):
+        self.write(self.temp_query)
+        resp1 = self.read(8, self.read_timeout)
+        resp2 = self.read(8, self.read_timeout)
+        if not resp1:
+            return None, None
+        int_temp = self._read_temp(resp1)
+        if not resp2:
+            ext_temp = None
+        else:
+            ext_temp = self._read_temp(resp2)
+            if ext_temp == 200:
+                # No external temperature recorded
+                ext_temp = None
+        return int_temp, ext_temp
+
+    def write(self, data):
+        self.h.write(data)
+
+    def read(self, size, timeout=0):
+        return self.h.read(size, timeout)
+
+    @staticmethod
+    def _read_string(b):
+        return ''.join([chr(r) for r in b])
+
+    @staticmethod
+    def _read_temp(b):
+        return ((b[2] << 8) + b[3]) / 100
+
+
+if __name__ == '__main__':
+    with Temper2() as t:
+        name = t.get_name()
+        print(name)
+        while True:
+            int_temp, ext_temp = t.get_temp()
+            print(int_temp, ext_temp)
+            time.sleep(1)
